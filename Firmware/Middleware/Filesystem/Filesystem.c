b9ca8c560a3677c8619eae8df1ebf10cf349b2b4
Kunal Salvi <kunalsalvi63@gmail.com>
2025-05-05 22:18:56 -0500

    Added Filesystem

---
+++ Firmware/Middleware/Filesystem/Filesystem.c
@@ -0,0 +1,8 @@
+/*
+ * Filesystem.c
+ *
+ *  Created on: May 5, 2025
+ *      Author: kunal
+ */
+
+
---
+++ Firmware/Middleware/Filesystem/Filesystem.h
@@ -0,0 +1,27 @@
+/*
+ * Filesystem.h
+ *
+ *  Created on: May 5, 2025
+ *      Author: kunal
+ */
+
+#ifndef FILESYSTEM_FILESYSTEM_H_
+#define FILESYSTEM_FILESYSTEM_H_
+
+#include "main.h"
+
+
+
+typedef struct FileSystem_Typedef
+{
+	uint8_t ID;
+	char *Name;
+	uint8_t Size;
+	uint8_t Starting_Address;
+
+}FileSystem_Typedef;
+
+
+
+
+#endif /* FILESYSTEM_FILESYSTEM_H_ */
